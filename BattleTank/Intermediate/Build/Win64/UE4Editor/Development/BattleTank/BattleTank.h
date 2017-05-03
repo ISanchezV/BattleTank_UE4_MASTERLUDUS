5b1e51c595dbae81436f10f09f9b97ff54be65ef
Inés <i.sanchez@ludus-academie.com>
2017-05-03 14:32:52 +0200

    Landscape, textures, etc

---
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Development/BattleTank/BattleTank.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_BATTLETANK_H__
+#define __AUTO_BATTLETANK_H__
+//Last Write: 26/04/2017 13:06:44 +00:00
+#include "D:\Trabajos\Ludus\Unreal\BattleTank_MasterLudusV1\BattleTank\Source\BattleTank\BattleTank.h"
+#endif//__AUTO_BATTLETANK_H__
---
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Development/BattleTank/PCH.BattleTank.h.cpp
@@ -0,0 +1,1 @@
+#include "D:\Trabajos\Ludus\Unreal\BattleTank_MasterLudusV1\BattleTank\Source\BattleTank\BattleTank.h"
